c5d1a5f5e025bc5b6c4002ae94767eb17cc1b375
Joseph C. DiMaggio <success1@gmail.com>
2018-03-18 10:33:54 -0400

    Added additional yargs functionality

--- app.js
+++ app.js
@@ -4,11 +4,34 @@ const yargs     = require('yargs');
 
 const notes     = require('./notes.js');
 
-const argv = yargs.argv;
-var command = argv._[0];
+const titleOptions = {
+    describe: 'Title of note',
+    demand: true,
+    alias: 't'
+};
 
-// console.log (`Command: ${command}`);
-// console.log('Yargs: ', argv);
+const bodyOptions = {
+    describe: 'Body of note',
+    demand: true,
+    alias: 'b'
+};
+
+const argv = yargs
+    .command('add', 'Add a new note', {
+        title: titleOptions,
+        body: bodyOptions
+    })
+    .command('list', 'List all notes')
+    .command('read', 'Read a note', {
+        title: titleOptions
+    })
+    .command('remove', 'Remove a note', {
+        title: titleOptions
+    })
+    .help()
+    .argv;
+
+var command = argv._[0];
 
 if (command === 'add') {
     var note = notes.addNote(argv.title, argv.body);
